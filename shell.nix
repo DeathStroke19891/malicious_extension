38898100a149a03b1cc833e64ee163fc8ba0a181
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-05-31 11:16:38 +0530

    Change nix files

--- shell.nix
+++ shell.nix
@@ -1,16 +1,11 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell
 {
-  description = "SDL2 tutorial";
+  nativeBuildInputs = [
+  ];
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells.${system}.default = (import ./shell.nix { inherit pkgs;});
-  };
+  shellHook = ''
+    alias rm="trash -c always put"
+  '';
 }
